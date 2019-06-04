748d2ee1814ea17ca230d49f45a705231840f8e0
dztt <dztt@email.com>
2019-06-05 00:27:46 +0800

    netty decoder

---
+++ chapter02_io/src/main/java/com/dztt/netty/frame/correct/TimeServerHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.dztt.netty.frame.correct;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.util.Date;
+
+/**
+ * @author lilinfeng
+ * @version 1.0
+ * @date 2014年2月14日
+ */
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //已被前面的StringDecoder 转换为字符串
+        String body = (String) msg;
+        System.out.println("The time server receive order : " + body + " ; the counter is : " + ++counter);
+
+        String dateStr = new Date(System.currentTimeMillis()).toString();
+        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? dateStr : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.writeAndFlush(resp);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
