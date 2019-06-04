0f83cc8dfcba2c1dc548d820daae2c06936b4293
dztt <dztt@email.com>
2019-06-05 01:43:39 +0800

    netty marshalling codec

---
+++ chapter02_io/src/main/java/com/dztt/netty/codec/marshalling/SubReqClientHandler.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.dztt.netty.codec.marshalling;
+
+import com.dztt.netty.codec.msgpack.User;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+
+/**
+ * @author lilinfeng
+ * @version 1.0
+ * @date 2014年2月14日
+ */
+public class SubReqClientHandler extends ChannelHandlerAdapter {
+
+    /**
+     * Creates a client-side handler.
+     */
+    public SubReqClientHandler() {
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        for (int i = 0; i < 10; i++) {
+            ctx.write(subReq(i));
+        }
+        ctx.flush();
+    }
+
+    private User subReq(int i) {
+        User req = new User();
+        req.setName("NanJing");
+        req.setAge(1);
+        return req;
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+        System.out.println("Receive server response : [" + msg + "]");
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
